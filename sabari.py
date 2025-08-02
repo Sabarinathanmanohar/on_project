854ac4ac8651a6195e64be66202457cd79c0ea6b
Sabarinathanmanohar <sabarimanohar84@gamil,com>
2025-08-02 23:51:16 +0530

    edited version

---
+++ sabari.py
@@ -0,0 +1,6 @@
+a=10
+b=15
+print("The sum of a and b is:", a + b)
+print("The difference of a and b is:", b - a)
+print("The product of a and b is:", a * b)
+print("The quotient of b and a is:", b / a)
